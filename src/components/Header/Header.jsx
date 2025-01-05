b595fa995ef652a086c68e3268a3531d09a413d0
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-05 20:31:44 +0300

    feat: nav styles and markup are placed in a separate component. (Sun, Jan 5, 2025, 8:31:38 PM)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,97 +1,27 @@
-import styles from "./Header.module.scss"
-import { NavLink } from "react-router-dom"
 import { useState } from "react"
 import HeaderTop from "./HeaderTop/HeaderTop"
+import { Nav } from "./Nav/Nav"
 
 const Header = () => {
 	const [isBurgerActive, setIsBurgerActive] = useState(false)
-	const [openMenu, setOpenMenu] = useState(null)
+	const [isMenuOpen, setIsMenuOpen] = useState(null)
 
 	const handleBurgerClick = () => {
 		setIsBurgerActive((prev) => !prev)
 	}
 
 	const handleMenuToggle = (index) => {
-		setOpenMenu((prev) => (prev === index ? null : index))
+		setIsMenuOpen((prev) => (prev === index ? null : index))
 	}
 
 	return (
-		<header className={styles.header}>
+		<header className="header">
 			<div className="container">
 				<HeaderTop
 					onBurgerClick={handleBurgerClick}
 					isBurgerActive={isBurgerActive}
 				/>
-				<nav className={styles.nav}>
-					<ul className={styles.nav__list}>
-						<li className={styles.nav__item}>
-							<NavLink className={styles["nav__item-link"]}>Товары</NavLink>
-						</li>
-						<li className={styles.nav__item}>
-							<NavLink className={styles["nav__item-link"]}>Портфолио</NavLink>
-						</li>
-						<li
-							className={`${styles["nav__item"]} ${styles["nav__dropdown-item"]}`}
-							onClick={() => handleMenuToggle(0)}
-						>
-							<div className={styles["nav__link-wrapper"]}>
-								<NavLink className={styles["nav__item-link"]}>
-									Покупателям
-								</NavLink>
-								<span
-									className={`${styles["nav__item-arrow"]} ${openMenu === 0 ? styles["open"] : ""}`}
-								>
-									▼
-								</span>
-							</div>
-
-							<ul
-								className={`${styles.nav__menu} ${openMenu === 0 ? styles["open"] : ""}`}
-							>
-								<li className={styles["nav__menu-item"]}>
-									<NavLink className={styles["nav__menu-link"]}>
-										Доставка и оплата
-									</NavLink>
-								</li>
-								<li className={styles["nav__menu-item"]}>
-									<NavLink className={styles["nav__menu-link"]}>
-										Стоимость
-									</NavLink>
-								</li>
-								<li className={styles["nav__menu-item"]}>
-									<NavLink className={styles["nav__menu-link"]}>
-										Полезные статьи и новости
-									</NavLink>
-								</li>
-							</ul>
-						</li>
-						<li
-							className={`${styles["nav__item"]} ${styles["nav__dropdown-item"]}`}
-							onClick={() => handleMenuToggle(1)}
-						>
-							<div className={styles["nav__link-wrapper"]}>
-								<NavLink className={styles["nav__item-link"]}>
-									О компании
-								</NavLink>
-								<span
-									className={`${styles["nav__item-arrow"]} ${openMenu === 1 ? styles["open"] : ""}`}
-								>
-									▼
-								</span>
-							</div>
-
-							<ul
-								className={`${styles.nav__menu} ${openMenu === 1 ? styles["open"] : ""}`}
-							>
-								<li className={styles["nav__menu-item"]}>
-									<NavLink className={styles["nav__menu-link"]}>
-										Наши контакты
-									</NavLink>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
+				<Nav onMenuClick={handleMenuToggle} isMenuOpen={isMenuOpen} />
 			</div>
 		</header>
 	)
---
+++ src/components/Header/Nav/Nav.jsx
@@ -0,0 +1,71 @@
+import { NavLink } from "react-router-dom"
+import styles from "./Nav.module.scss"
+
+export const Nav = ({ handleMenuToggle, isMenuOpen }) => {
+	return (
+		<nav className={styles.nav}>
+			<ul className={styles.nav__list}>
+				<li className={styles.nav__item}>
+					<NavLink className={styles["nav__item-link"]}>Товары</NavLink>
+				</li>
+				<li className={styles.nav__item}>
+					<NavLink className={styles["nav__item-link"]}>Портфолио</NavLink>
+				</li>
+				<li
+					className={`${styles["nav__item"]} ${styles["nav__dropdown-item"]}`}
+					onClick={() => handleMenuToggle(0)}
+				>
+					<div className={styles["nav__link-wrapper"]}>
+						<NavLink className={styles["nav__item-link"]}>Покупателям</NavLink>
+						<span
+							className={`${styles["nav__item-arrow"]} ${isMenuOpen === 0 ? styles["open"] : ""}`}
+						>
+							▼
+						</span>
+					</div>
+
+					<ul
+						className={`${styles.nav__menu} ${isMenuOpen === 0 ? styles["open"] : ""}`}
+					>
+						<li className={styles["nav__menu-item"]}>
+							<NavLink className={styles["nav__menu-link"]}>
+								Доставка и оплата
+							</NavLink>
+						</li>
+						<li className={styles["nav__menu-item"]}>
+							<NavLink className={styles["nav__menu-link"]}>Стоимость</NavLink>
+						</li>
+						<li className={styles["nav__menu-item"]}>
+							<NavLink className={styles["nav__menu-link"]}>
+								Полезные статьи и новости
+							</NavLink>
+						</li>
+					</ul>
+				</li>
+				<li
+					className={`${styles["nav__item"]} ${styles["nav__dropdown-item"]}`}
+					onClick={() => handleMenuToggle(1)}
+				>
+					<div className={styles["nav__link-wrapper"]}>
+						<NavLink className={styles["nav__item-link"]}>О компании</NavLink>
+						<span
+							className={`${styles["nav__item-arrow"]} ${isMenuOpen === 1 ? styles["open"] : ""}`}
+						>
+							▼
+						</span>
+					</div>
+
+					<ul
+						className={`${styles.nav__menu} ${isMenuOpen === 1 ? styles["open"] : ""}`}
+					>
+						<li className={styles["nav__menu-item"]}>
+							<NavLink className={styles["nav__menu-link"]}>
+								Наши контакты
+							</NavLink>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	)
+}
