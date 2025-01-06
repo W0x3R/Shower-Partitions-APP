d6ef3dc5f67076b4ed90c7f88d649144901dce0a
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-06 21:45:42 +0300

    feat: added and connected background images for header and main. added styles for images. (Mon, Jan 6, 2025, 9:45:39 PM)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,21 @@
+import styles from "./Main.module.scss"
+import mainBackgroundDesktop from "../../assets/main/main-bg-big.webp"
+import mainBackgroundMobile from "../../assets/main/main-bg-small.webp"
 import { About } from "./About/About"
 
 export const Main = () => {
 	return (
 		<main>
+			<div className={styles.main__background}>
+				<img
+					className={styles["main__background-desktop"]}
+					src={mainBackgroundDesktop}
+				></img>
+				<img
+					className={styles["main__background-mobile"]}
+					src={mainBackgroundMobile}
+				></img>
+			</div>
 			<div className="container">
 				<About />
 			</div>
