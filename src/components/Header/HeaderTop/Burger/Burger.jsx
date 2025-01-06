01c2fa82a0d01d3dfca48bd8cbc70057fe11b614
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-06 19:08:57 +0300

    refactor: burger component refactoring was performed (Mon, Jan 6, 2025, 7:08:51 PM)

--- src/components/Header/HeaderTop/Burger/Burger.jsx
+++ src/components/Header/HeaderTop/Burger/Burger.jsx
@@ -1,17 +1,18 @@
 import styles from "./Burger.module.scss"
 
 export const Burger = ({ isBurgerActive, onBurgerClick }) => {
+	const lines = Array(3).fill(null)
+
 	return (
 		<div className={styles.burger} onClick={onBurgerClick}>
-			<span
-				className={`${styles.burger__line} ${isBurgerActive ? styles.active : ""}`}
-			></span>
-			<span
-				className={`${styles.burger__line} ${isBurgerActive ? styles.active : ""}`}
-			></span>
-			<span
-				className={`${styles.burger__line} ${isBurgerActive ? styles.active : ""}`}
-			></span>
+			{lines.map((_, i) => {
+				return (
+					<span
+						key={i}
+						className={`${styles.burger__line} ${isBurgerActive ? styles.active : ""}`}
+					></span>
+				)
+			})}
 		</div>
 	)
 }
