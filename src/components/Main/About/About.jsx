954ec289af223503671557d692eea51a878e74a2
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-07 17:03:04 +0300

    feat: added about component, markup and styles for it. (Tue, Jan 7, 2025, 5:02:49 PM)

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,21 +1,59 @@
 import styles from "./About.module.scss"
+import featureCircle from "../../../assets/main/feature-circle.svg?url"
+import clickHand from "../../../assets/main/click-hand.svg?url"
 
 export const About = () => {
 	return (
 		<section className={styles.about}>
-			<h1 className={styles.about__title}>Душевые перегородки на заказ</h1>
+			<h1 className={styles.about__title}>
+				<b>Душевые перегородки</b> на заказ
+			</h1>
 			<div className={styles.about__description}>
-				<div className={styles["about__description-dots"]}></div>
-				<p className={styles["about__description-text"]}></p>
+				<p className={styles["about__description-text"]}>
+					Стильные ограждения для душевой премиум качества
+				</p>
 			</div>
 			<div className={styles.about__features}>
-				<div className={styles["about__features-item"]}></div>
-				<div className={styles["about__features-item"]}></div>
-				<div className={styles["about__features-item"]}></div>
+				<div className={styles["about__features-item"]}>
+					<img src={featureCircle} />
+					<p className={styles["about__features-text"]}>
+						<b>Закаленное стекло</b> <br />
+						8-10 мм
+					</p>
+				</div>
+				<div className={styles["about__features-item"]}>
+					<img src={featureCircle} />
+					<p className={styles["about__features-text"]}>
+						<b>Дизайнерские решения.</b>
+					</p>
+				</div>
+				<div className={styles["about__features-item"]}>
+					<img src={featureCircle} />
+					<p className={styles["about__features-text"]}>
+						<b>
+							Тонкая <br />
+							рамка
+						</b>
+					</p>
+				</div>
+				<div className={styles["about__features-item"]}>
+					<img src={featureCircle} />
+					<p className={styles["about__features-text"]}>
+						<b>
+							Надежная <br />
+							фурнитура
+						</b>
+					</p>
+				</div>
 			</div>
 			<div className={styles.about__order}>
-				<button className={styles["about__order-btn"]}></button>
-				<p className={styles["about__order-text"]}></p>
+				<button className={styles["about__order-btn"]}>
+					Рассчитать цену по моим размерам
+					<img src={clickHand} />
+				</button>
+				<p className={styles["about__order-text"]}>
+					<b>Перезвоним и проконсультируем </b>
+				</p>
 			</div>
 		</section>
 	)
