bb3cfdc8e71d8f245a5a4074b6ab7925d3d5b142
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-12 18:07:46 +0300

    feat: the disablebodyscroll and enablebodyscroll functions have been renamed. new functions disablebodyscroll and enablebodyscroll created  (Sun, Jan 12, 2025, 6:07:38 PM)

--- src/utils/setBodyScroll.js
+++ src/utils/setBodyScroll.js
@@ -1,15 +1,23 @@
-const body = document.body
+export const body = document.body
 
-export const disableBodyScroll = () => {
+export const disableBodyScrollIncludeScrollbar = () => {
 	const scrollBarWidth =
 		window.innerWidth - document.documentElement.clientWidth
 	body.style.overflow = "hidden"
 	body.style.paddingRight = `${scrollBarWidth}px`
 }
 
-export const enableBodyScroll = () => {
+export const enableBodyScrollIncludeScrollbar = () => {
 	setTimeout(() => {
 		body.style.overflow = ""
 		body.style.paddingRight = ""
 	}, 300)
+}
+
+export const disableBodyScroll = () => {
+	body.style.overflow = "hidden"
+}
+
+export const enableBodyScroll = () => {
+	body.style.overflow = ""
 }
