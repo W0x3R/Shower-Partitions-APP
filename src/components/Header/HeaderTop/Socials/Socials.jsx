0a8776d3c90ac84b0de3cd52614e4fd85a49ce5c
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-14 15:04:04 +0300

    refactor: the b tag has been replaced with strong (Tue, Jan 14, 2025, 3:03:58 PM)

--- src/components/Header/HeaderTop/Socials/Socials.jsx
+++ src/components/Header/HeaderTop/Socials/Socials.jsx
@@ -5,7 +5,7 @@ export const Socials = () => {
 	return (
 		<div className={styles.socials}>
 			<p className={`header__animation header__texts`}>
-				<b>Задайте вопрос, мы онлайн:</b>
+				<strong>Задайте вопрос, мы онлайн:</strong>
 			</p>
 			<SocialLinks gap="socials-header" sizes="socials__link-sizes" />
 		</div>
