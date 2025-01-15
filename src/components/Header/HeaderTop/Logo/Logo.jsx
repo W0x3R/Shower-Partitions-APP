babc698e734710ac27ccbcd519e74b468632a16b
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-15 14:54:47 +0300

    refactor: removed old logo and added optimized new logo (Wed, Jan 15, 2025, 2:54:43 PM)

--- src/components/Header/HeaderTop/Logo/Logo.jsx
+++ src/components/Header/HeaderTop/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import logo from "../../../../assets/header/logo.jpg"
+import logo from "../../../../assets/header/logo.webp"
 import styles from "./Logo.module.scss"
 
 export const Logo = () => {
