1c6fb690056248323752e9a20ae06bed62d16583
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-15 16:47:38 +0300

    feat: added aria-labelledby and id (Wed, Jan 15, 2025, 4:47:30 PM)

--- src/components/Header/HeaderTop/Socials/Socials.jsx
+++ src/components/Header/HeaderTop/Socials/Socials.jsx
@@ -3,8 +3,8 @@ import styles from "./Socials.module.scss"
 
 export const Socials = () => {
 	return (
-		<div className={styles.socials}>
-			<p className={`header__animation header__texts`}>
+		<div className={styles.socials} aria-labelledby="online-question">
+			<p className={`header__animation header__texts`} id="online-question">
 				<strong>Задайте вопрос, мы онлайн:</strong>
 			</p>
 			<SocialLinks gap="socials-header" sizes="socials__link-sizes" />
