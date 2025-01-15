401919f2646a6f101fca7e5a8a1c0389411dbf09
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-15 16:56:43 +0300

    feat: added aria-labelledby, id. changed title. added button type (Wed, Jan 15, 2025, 4:56:39 PM)

--- src/components/Header/HeaderTop/CallUs/CallUs.jsx
+++ src/components/Header/HeaderTop/CallUs/CallUs.jsx
@@ -2,8 +2,8 @@ import styles from "./CallUs.module.scss"
 
 export const CallUs = () => {
 	return (
-		<div className={styles.callUs}>
-			<p className={`header__texts header__animation`}>
+		<div className={styles.callUs} aria-labelledby="working-time">
+			<p className={`header__texts header__animation`} id="working-time">
 				<strong>
 					<span>Звоните Пн-Пт</span> 9 - 18
 				</strong>
@@ -12,11 +12,12 @@ export const CallUs = () => {
 				className={styles.callUs__link}
 				href="tel:+375447625856"
 				aria-label="Связаться с нами по номеру телефона"
-				title="Позвонить по номеру +375 (44) 762-58-56"
+				title="Позвонить по номеру +375 44 762 58 56"
 			>
 				<strong>+375 (44) 762-58-56</strong>
 			</a>
 			<button
+				type="button"
 				className={styles.callUs__btn}
 				aria-label="Нажмите, чтобы заказать звонок"
 			>
