14f63e61e140aa74d02f2e843d4ccf09dd5a725e
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-18 17:59:59 +0300

    refactor: the click event has been moved to the link (Sat, Jan 18, 2025, 5:59:56 PM)

--- src/components/Header/Nav/NavItems.jsx
+++ src/components/Header/Nav/NavItems.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom"
 
-export const NavItems = ({ styles, titleItems }) => {
+export const NavItems = ({ styles, titleItems, onBurgerClick }) => {
 	return (
 		<>
 			{titleItems.map((navItem) => {
 				return (
 					<li className={styles.nav__item} key={navItem}>
-						<NavLink className={styles["nav__item-link"]}>{navItem}</NavLink>
+						<NavLink
+							className={styles["nav__item-link"]}
+							onClick={onBurgerClick}
+						>
+							{navItem}
+						</NavLink>
 					</li>
 				)
 			})}
