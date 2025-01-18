f8aed8f3c3fb656295547c3f6ecd8008132846a9
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-18 18:31:36 +0300

    feat: the sr-only class has been added. added accessibility attributes (Sat, Jan 18, 2025, 6:31:33 PM)

--- src/components/Header/HeaderTop/CallUs/CallUs.jsx
+++ src/components/Header/HeaderTop/CallUs/CallUs.jsx
@@ -3,10 +3,17 @@ import styles from "./CallUs.module.scss"
 export const CallUs = () => {
 	return (
 		<div className={styles.callUs} aria-labelledby="working-time">
-			<p className={`header__texts header__animation`} id="working-time">
-				<strong>
+			<p
+				className="header__texts header__animation"
+				tabIndex={0}
+				aria-labelledby="working-time-description"
+			>
+				<strong aria-hidden>
 					<span>Звоните Пн-Пт</span> 9 - 18
 				</strong>
+				<span id="working-time-description" className="sr-only">
+					Мы работаем с понедельника по пятницу с 9 утра до 6 часов вечера
+				</span>
 			</p>
 			<a
 				className={styles.callUs__link}
