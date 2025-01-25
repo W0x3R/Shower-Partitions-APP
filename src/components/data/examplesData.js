e741549ceb92f695712dc9fad7b45938152c63a7
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-25 20:32:29 +0300

    feat: added data for adding product example cards (Sat, Jan 25, 2025, 8:32:26 PM)

---
+++ src/components/data/examplesData.js
@@ -0,0 +1,149 @@
+import firstExampleMain from "../../assets/main/first-example-main.webp"
+import firstExampleSlider from "../../assets/main/first-example-slider.webp"
+import secondExampleMain from "../../assets/main/second-example-main.webp"
+import secondExampleSlider from "../../assets/main/second-example-slider.webp"
+import thirdExampleMain from "../../assets/main/third-example-main.webp"
+import thirdExampleSlider from "../../assets/main/third-example-slider.webp"
+import fourthExampleMain from "../../assets/main/fourth-example-main.webp"
+import fourthExampleSlider from "../../assets/main/fourth-example-slider.webp"
+import fifthExampleMain from "../../assets/main/fifth-example-main.webp"
+import fifthExampleSlider from "../../assets/main/fifth-example-slider.webp"
+import sixthExampleMain from "../../assets/main/sixth-example-main.webp"
+import sixthExampleSlider from "../../assets/main/sixth-example-slider.webp"
+import seventhExampleMain from "../../assets/main/seventh-example-main.webp"
+import seventhExampleSlider from "../../assets/main/seventh-example-slider.webp"
+import eighthExampleMain from "../../assets/main/eighth-example-main.webp"
+import eighthExampleSlider from "../../assets/main/eighth-example-slider.webp"
+import ninthExampleMain from "../../assets/main/ninth-example-main.webp"
+import ninthExampleSlider from "../../assets/main/ninth-example-slider.webp"
+import tenthExampleMain from "../../assets/main/tenth-example-main.webp"
+import tenthExampleSlider from "../../assets/main/tenth-example-slider.webp"
+import eleventhExampleMain from "../../assets/main/eleventh-example-main.webp"
+import eleventhExampleSlider from "../../assets/main/eleventh-example-slider.webp"
+import twelfthExampleMain from "../../assets/main/twelfth-example-main.webp"
+import twelfthExampleSlider from "../../assets/main/twelfth-example-slider.webp"
+import thirteenthExampleMain from "../../assets/main/thirteenth-example-main.webp"
+import thirteenthExampleSlider from "../../assets/main/thirteenth-example-slider.webp"
+import fourteenthExampleMain from "../../assets/main/fourteenth-example-main.webp"
+import fourteenthExampleSlider from "../../assets/main/fourteenth-example-slider.webp"
+import fifteenthExampleMain from "../../assets/main/fifteenth-example-main.webp"
+import fifteenthExampleSlider from "../../assets/main/fifteenth-example-slider.webp"
+import sixteenthExampleMain from "../../assets/main/sixteenth-example-main.webp"
+import sixteenthExampleSlider from "../../assets/main/sixteenth-example-slider.webp"
+import seventeenthExampleMain from "../../assets/main/seventeenth-example-main.webp"
+import seventeenthExampleSlider from "../../assets/main/seventeenth-example-slider.webp"
+import eighteenthExampleMain from "../../assets/main/eighteenth-example-main.webp"
+import eighteenthExampleSlider from "../../assets/main/eighteenth-example-slider.webp"
+
+const examplesData = [
+	{
+		mainImg: firstExampleMain,
+		sliderImg: firstExampleSlider,
+		alt: "На изображении отображена трапециевидная душевая перегородка на фоне комнаты, оформленной в бело-черных тонах",
+		id: 0,
+	},
+	{
+		mainImg: secondExampleMain,
+		sliderImg: secondExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне комнаты, оформленной в светло-бежевых тонах",
+		id: 1,
+	},
+	{
+		mainImg: thirdExampleMain,
+		sliderImg: thirdExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне комнаты, оформленной в светло-серых тонах",
+		id: 2,
+	},
+	{
+		mainImg: fourthExampleMain,
+		sliderImg: fourthExampleSlider,
+		alt: "На изображении отображена душевая перегородка в нишу на фоне ванной комнаты, оформленной в светлых тонах",
+		id: 3,
+	},
+	{
+		mainImg: fifthExampleMain,
+		sliderImg: fifthExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне комнаты, оформленной в светло-серых тонах",
+		id: 4,
+	},
+	{
+		mainImg: sixthExampleMain,
+		sliderImg: sixthExampleSlider,
+		alt: "На изображении отображена трапециевидная душевая перегородка на фоне комнаты, оформленной в светло-серых тонах",
+		id: 5,
+	},
+	{
+		mainImg: seventhExampleMain,
+		sliderImg: seventhExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне ванной комнаты, оформленной в светло-серых тонах",
+		id: 6,
+	},
+	{
+		mainImg: eighthExampleMain,
+		sliderImg: eighthExampleSlider,
+		alt: "На изображении отображена душевая перегородка в нишу на фоне ванной комнаты, оформленной в бежевых тонах",
+		id: 7,
+	},
+	{
+		mainImg: ninthExampleMain,
+		sliderImg: ninthExampleSlider,
+		alt: "На изображении отображена трапециевидная душевая перегородка на фоне ванной комнаты, оформленной в светло-коричневых тонах",
+		id: 8,
+	},
+	{
+		mainImg: tenthExampleMain,
+		sliderImg: tenthExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне комнаты, оформленной в светлых тонах",
+		id: 9,
+	},
+	{
+		mainImg: eleventhExampleMain,
+		sliderImg: eleventhExampleSlider,
+		alt: "На изображении отображена душевая перегородка в нишу на фоне ванной комнаты, оформленной в светлых тонах",
+		id: 10,
+	},
+	{
+		mainImg: twelfthExampleMain,
+		sliderImg: twelfthExampleSlider,
+		alt: "На изображении отображена стеклянная шторка в ванную",
+		id: 11,
+	},
+	{
+		mainImg: thirteenthExampleMain,
+		sliderImg: thirteenthExampleSlider,
+		alt: "На изображении отображена угловая душевая кабина на фоне ванной комнаты, оформленной в светлых тонах",
+		id: 12,
+	},
+	{
+		mainImg: fourteenthExampleMain,
+		sliderImg: fourteenthExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне комнаты, оформленной в светлых тонах",
+		id: 13,
+	},
+	{
+		mainImg: fifteenthExampleMain,
+		sliderImg: fifteenthExampleSlider,
+		alt: "На изображении отображена душевая перегородка в нишу на фоне комнаты, оформленной в светлых тонах",
+		id: 14,
+	},
+	{
+		mainImg: sixteenthExampleMain,
+		sliderImg: sixteenthExampleSlider,
+		alt: "На изображении отображена стеклянная шторка в ванную",
+		id: 15,
+	},
+	{
+		mainImg: seventeenthExampleMain,
+		sliderImg: seventeenthExampleSlider,
+		alt: "На изображении отображена душевая кабина на фоне ванной комнаты, оформленной в светло-сиреневых тонах",
+		id: 16,
+	},
+	{
+		mainImg: eighteenthExampleMain,
+		sliderImg: eighteenthExampleSlider,
+		alt: "На изображении отображена трапециевидная душевая перегородка на фоне комнаты, оформленной в темно-светлых тонах",
+		id: 17,
+	},
+]
+
+export default examplesData
