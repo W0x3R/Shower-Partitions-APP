de383ea04388c2998cfe5d00bf39bf04c6fe29ef
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-01-25 20:34:26 +0300

    feat: examples component added to main (Sat, Jan 25, 2025, 8:34:22 PM)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import styles from "./Main.module.scss"
 
 import { About } from "./About/About"
 import { Products } from "./Products/Products"
+import { Examples } from "./Examples/Examples"
 
 export const Main = () => {
 	return (
 		<main className={styles.main}>
 			<About />
 			<Products />
+			<Examples />
 		</main>
 	)
 }
