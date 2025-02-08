16fe62bfd2681842a5be92b7cb24d196652f49fc
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-08 17:12:20 +0300

    feat: benefits component, data for it and styles have been created (Sat, Feb 8, 2025, 5:12:17 PM)

---
+++ src/Footer/Benefits/Benefits.jsx
@@ -0,0 +1,21 @@
+import { benefitsData } from "../../components/data/benefitsData"
+import CheckIcon from "../../assets/footer/check.svg?react"
+import styles from "./Benefits.module.scss"
+
+export const Benefits = () => {
+	return (
+		<div className={styles.benefits__wrapper}>
+			<h6 className={styles.benefits__title}>Преимущества</h6>
+			<ul className={styles.benefits__list}>
+				{benefitsData.map(({ id, text }) => {
+					return (
+						<li key={id} className={styles.benefits__item}>
+							<CheckIcon className="footer__icon" />
+							{text}
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
---
+++ src/components/data/benefitsData.js
@@ -0,0 +1,26 @@
+export const benefitsData = [
+	{
+		id: "benefits_0",
+		text: "Работаем под ключ",
+	},
+	{
+		id: "benefits_1",
+		text: "Опытные монтажники",
+	},
+	{
+		id: "benefits_2",
+		text: "Бесплатный 3D проект",
+	},
+	{
+		id: "benefits_3",
+		text: "Без предоплаты",
+	},
+	{
+		id: "benefits_4",
+		text: "Гарантия 18 месяцев",
+	},
+	{
+		id: "benefits_5",
+		text: "Беспроцентная рассрочка",
+	},
+]
