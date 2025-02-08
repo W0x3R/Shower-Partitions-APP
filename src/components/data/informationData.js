facb35dd66e329c9e651de06b9271a693c5d16ef
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-08 17:13:49 +0300

    feat: added data for the information component (Sat, Feb 8, 2025, 5:13:46 PM)

---
+++ src/components/data/informationData.js
@@ -0,0 +1,30 @@
+export const informationData = [
+	{
+		id: "information_0",
+		text: `ООО "ГАРДЕРОБНЫЕ СИСТЕМЫ"`,
+	},
+	{
+		id: "information_1",
+		text: `ИНН: 9729295609`,
+	},
+	{
+		id: "information_2",
+		text: `КПП: 772901001`,
+	},
+	{
+		id: "information_3",
+		text: `ОГРН: 1207700138656`,
+	},
+	{
+		id: "information_4",
+		text: `Юридический адрес: 119602, г. Москва`,
+	},
+	{
+		id: "information_5",
+		text: `Тропарёвская ул., влд. 4 стр. 1, этаж 4`,
+	},
+	{
+		id: "information_6",
+		text: `помещ. VIIа, комната 460`,
+	},
+]
