3a83e38fe62f504ba6adf1df4ef87839c9558fdb
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-11 21:28:56 +0300

    feat: used helmet for metadata (Tue, Feb 11, 2025, 9:28:53 PM)

--- src/components/Main/MainPage.jsx
+++ src/components/Main/MainPage.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet"
 import { About } from "./About/About"
 import { Products } from "./Products/Products"
 import { Examples } from "./Examples/Examples"
@@ -11,6 +12,15 @@ import ContactsWidget from "./ContactsWidget/ContactsWidget"
 export const MainPage = () => {
 	return (
 		<>
+			<Helmet>
+				<title>
+					Главная | Стеклянные душевые перегородки на заказ в Гомеле
+				</title>
+				<meta
+					name="description"
+					content="Душевые перегородки на заказ по индивидуальным размерам в Гомеле: замер, изготовление, установка. Современный дизайн, закаленное стекло(8-10мм), надежная фурнитура, гарантия!"
+				/>
+			</Helmet>
 			<About />
 			<Products />
 			<Examples />
