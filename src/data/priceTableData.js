cb894a70a781c9a834344014344a95ab8d9e1468
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-16 21:35:50 +0300

    refactor: the text data is placed in separate files (Sun, Feb 16, 2025, 9:35:46 PM)

---
+++ src/data/priceTableData.js
@@ -0,0 +1,58 @@
+const priceTableData = [
+	{
+		id: "table_0",
+		name: "Доставка Гомель",
+		cost: "0р",
+		extraInfo: "+1р/км за городом",
+	},
+	{
+		id: "table_1",
+		name: "Замер при заказе",
+		cost: "0р",
+		extraInfo: "+1р/км за городом",
+	},
+	{
+		id: "table_2",
+		name: "Замер без заказа",
+		cost: "120р",
+		extraInfo: "+1р/км за городом",
+	},
+	{
+		id: "table_3",
+		name: "Монтаж шторки в ванную",
+		cost: "От 180р",
+		extraInfo: "",
+	},
+	{
+		id: "table_4",
+		name: "Монтаж душевой перегородки",
+		cost: "От 380р",
+		extraInfo: "",
+	},
+	{
+		id: "table_5",
+		name: "Монтаж душевой в нишу",
+		cost: "От 280р",
+		extraInfo: "",
+	},
+	{
+		id: "table_6",
+		name: "Монтаж угловой душевой",
+		cost: "От 380р",
+		extraInfo: "",
+	},
+	{
+		id: "table_7",
+		name: "Монтаж трапециевидной душевой",
+		cost: "От 380р",
+		extraInfo: "",
+	},
+	{
+		id: "table_8",
+		name: "Монтаж душевой кабины",
+		cost: "От 380р",
+		extraInfo: "",
+	},
+]
+
+export default priceTableData
