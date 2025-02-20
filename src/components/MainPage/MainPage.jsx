73dad4da50256eb80cfd5248484b7213670965c5
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-20 14:14:10 +0300

    feat: changed and moved meta tags (Thu, Feb 20, 2025, 2:14:05 PM)

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet"
+import { useLocation } from "react-router-dom"
 import { About } from "./About/About"
 import { Products } from "./Products/Products"
 import { Examples } from "./Examples/Examples"
@@ -8,14 +9,22 @@ import { Standards } from "./Standards/Standards"
 import { ShowerPartitionsDescription } from "./ShowerPartitionsDescription/ShowerPartitionsDescription"
 import ScrollTopBtn from "../Widgets/ScrollTopBtn/ScrollTopBtn"
 import ContactsWidget from "../Widgets/ContactsWidget/ContactsWidget"
+import isMobile from "../../utils/isMobile"
+import smallBg from "../../assets/main/main-bg-small.webp"
+import bigBg from "../../assets/main/main-bg-big.webp"
 
 export const MainPage = () => {
+	const location = useLocation()
+	const currentUrl = `https://garderobsystem.ru${location.pathname}`
 	return (
 		<>
 			<Helmet>
+				{/* Title */}
 				<title>
-					Главная | Стеклянные душевые перегородки на заказ в Гомеле
+					Главная | Душевые перегородки по индивидуальным размерам на заказ в
+					Гомеле от DZ System
 				</title>
+				{/* Meta Tags */}
 				<meta
 					name="description"
 					content="Душевые перегородки на заказ по индивидуальным размерам в Гомеле: замер, изготовление, установка. Современный дизайн, закаленное стекло(8-10мм), надежная фурнитура, гарантия!"
@@ -24,6 +33,40 @@ export const MainPage = () => {
 					name="keywords"
 					content="DZ System,душевые перегородки Гомель, угловая душевая, перегородки для ванной,шторка в ванную, стеклянные конструкции, изготовление на заказ, монтаж и установка"
 				/>
+				{/* Meta OG */}
+				<meta
+					property="og:title"
+					content="Душевые перегородки по индивидуальным размерам на заказ в
+					Гомеле от DZ System"
+				/>
+				<meta
+					property="og:description"
+					content="Душевые перегородки на заказ по индивидуальным размерам в Гомеле: замер, изготовление, установка. Современный дизайн, закаленное стекло(8-10мм), надежная фурнитура, гарантия!"
+				/>
+				<meta
+					property="og:image"
+					content="https://W0x3R.github.io/Shower-Partitions-APP/og-image.jpg"
+				/>
+				<meta property="og:url" content={currentUrl} />
+				<meta property="og:type" content="website" />
+				{/* Meta Twitter */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta
+					name="twitter:title"
+					content="Душевые перегородки на заказ в Гомеле"
+				/>
+				<meta
+					name="twitter:description"
+					content="Душевые перегородки на заказ по индивидуальным размерам в Гомеле: замер, изготовление, установка. Современный дизайн, закаленное стекло(8-10мм), надежная фурнитура, гарантия!"
+				/>
+				<meta
+					name="twitter:image"
+					content="https://W0x3R.github.io/Shower-Partitions-APP/og-image.jpg"
+				/>
+				{/* Preloading images */}
+				{isMobile() ?
+					<link rel="preload" href={smallBg} as="image" type="image/webp" />
+				:	<link rel="preload" href={bigBg} as="image" type="image/webp" />}
 			</Helmet>
 			<About />
 			<Products />
