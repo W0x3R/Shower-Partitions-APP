185988b84320f8855565328938171d7a9fc5c753
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-20 12:02:26 +0300

    feat: created article component and styles for it (Thu, Feb 20, 2025, 12:02:21 PM)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./Layout/Layout"
 import { MainPage } from "./components/MainPage/MainPage"
 import { DeliveryPage } from "./components/Delivery/DeliveryPage"
 import { ServicesCostPage } from "./components/ServicesCost/ServicesCostPage"
+import { ArticlePage } from "./components/Article/ArticlePage"
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 					<Route index element={<MainPage />} />
 					<Route path="delivery" element={<DeliveryPage />} />
 					<Route path="price" element={<ServicesCostPage />} />
+					<Route path="article" element={<ArticlePage />} />
 				</Route>
 			</Routes>
 			<div className="App"></div>
---
+++ src/components/Article/ArticlePage.jsx
@@ -0,0 +1,9 @@
+import { Article } from "./Article"
+
+export const ArticlePage = () => {
+	return (
+		<>
+			<Article />
+		</>
+	)
+}
