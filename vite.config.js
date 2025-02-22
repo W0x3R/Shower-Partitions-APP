e72c5efa2a13daca4a0fae32100e50976723092b
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-22 17:35:28 +0300

    feat: preparing for test deployment (Sat, Feb 22, 2025, 5:35:20 PM)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { createHtmlPlugin } from "vite-plugin-html"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// base: "/Shower-Partitions-APP/",
+	base: "/Shower-Partitions-APP/",
 	plugins: [
 		react(),
 		svgo(),
