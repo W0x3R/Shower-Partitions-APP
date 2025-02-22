b744195fdd7124c7ff2272cdb0e231215681be42
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-02-22 20:47:34 +0300

    feat: changed config (Sat, Feb 22, 2025, 8:47:32 PM)

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,9 @@ export default defineConfig({
 		}),
 	],
 	build: {
+		outDir: "dist",
+		assetsDir: "assets",
+		emptyOutDir: true,
 		minify: "esbuild",
 		sourcemap: false,
 		cssCodeSplit: true,
