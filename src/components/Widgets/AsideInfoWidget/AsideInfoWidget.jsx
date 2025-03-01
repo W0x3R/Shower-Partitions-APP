e326aee4c44f4e01e46c10d423aab8d94f500498
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-03-01 13:59:03 +0300

    refactor: changed content and display of asideinfowidget (Sat, Mar 1, 2025, 1:58:49 PM)

--- src/components/Widgets/AsideInfoWidget/AsideInfoWidget.jsx
+++ src/components/Widgets/AsideInfoWidget/AsideInfoWidget.jsx
@@ -5,6 +5,25 @@ import clickHand from "../../../assets/main/click-hand.svg?url"
 export const AsideInfoWidget = ({ sticky }) => {
 	return (
 		<aside className={`${styles.aside} ${sticky ? styles.sticky : ""}`}>
+			<h3 className={styles.aside__title}>Заказать обратный звонок</h3>
+			<a
+				className={styles.aside__link}
+				href="#"
+				rel="noopener noreferrer"
+				aria-label="Заказать обратный звонок"
+			>
+				<span>Заказать</span>
+				<img src={clickHand} alt="" />
+			</a>
+			<h3 className={styles.aside__title}>Связаться с нами по телефону</h3>
+			<a
+				className={styles.aside__link}
+				href="tel:+375447625856"
+				aria-label="Связаться с нами по номеру телефона"
+			>
+				<span>Позвонить</span>
+				<img src={clickHand} alt="" />
+			</a>
 			<h3 className={styles.aside__title}>Каталог продукции и фурнитуры</h3>
 			<Link
 				className={styles.aside__link}
@@ -25,25 +44,6 @@ export const AsideInfoWidget = ({ sticky }) => {
 				<span>Посмотреть</span>
 				<img src={clickHand} alt="" />
 			</Link>
-			<h3 className={styles.aside__title}>Связаться с нами по телефону</h3>
-			<a
-				className={styles.aside__link}
-				href="tel:+375447625856"
-				aria-label="Связаться с нами по номеру телефона"
-			>
-				<span>Позвонить</span>
-				<img src={clickHand} alt="" />
-			</a>
-			<h3 className={styles.aside__title}>Связаться с нами в Viber</h3>
-			<a
-				className={styles.aside__link}
-				href="#"
-				rel="noopener noreferrer"
-				aria-label="Связаться с нами в Viber"
-			>
-				<span>Связаться</span>
-				<img src={clickHand} alt="" />
-			</a>
 		</aside>
 	)
 }
