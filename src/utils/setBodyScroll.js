c7a2b5c6ee2797ae47b6bcb2b5c33a85f3e10dbd
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-03-08 18:35:45 +0300

    feat: added event to disable scroll in safari (Sat, Mar 8, 2025, 6:35:41 PM)

--- src/utils/setBodyScroll.js
+++ src/utils/setBodyScroll.js
@@ -1,21 +1,26 @@
 export const body = document.body
+const disableScroll = (e) => e.preventDefault()
 
 export const disableBodyScrollIncludeScrollbar = () => {
 	const scrollBarWidth =
 		window.innerWidth - document.documentElement.clientWidth
 	body.style.overflow = "hidden"
+	document.addEventListener("touchmove", disableScroll, { passive: false })
 	body.style.paddingRight = `${scrollBarWidth}px`
 }
 
 export const enableBodyScrollIncludeScrollbar = () => {
 	body.style.overflow = ""
+	document.removeEventListener("touchmove", disableScroll)
 	body.style.paddingRight = ""
 }
 
 export const disableBodyScroll = () => {
 	body.style.overflow = "hidden"
+	document.addEventListener("touchmove", disableScroll, { passive: false })
 }
 
 export const enableBodyScroll = () => {
 	body.style.overflow = ""
+	document.removeEventListener("touchmove", disableScroll)
 }
