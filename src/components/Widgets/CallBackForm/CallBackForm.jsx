8c4c0b669c08250a4edd149a91bc13aafac696e9
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-05-11 20:38:55 +0300

    feat: added asterisks to indicate mandatory completion of data in the form (Sun, May 11, 2025, 8:38:50 PM)

--- src/components/Widgets/CallBackForm/CallBackForm.jsx
+++ src/components/Widgets/CallBackForm/CallBackForm.jsx
@@ -108,112 +108,122 @@ const CallBackForm = ({
 			<h2 className={styles.form__title} id={ariaLabelledBy}>
 				{title}
 			</h2>
-			<input
-				className={`${styles.form__input} ${errors.name?.message ? styles.resetIndent : ""}`}
-				{...register("name", {
-					required: "Имя обязательно",
-					validate: (value) => {
-						if (value.length < 2)
-							return "Имя должно содержать хотя бы 2 символа"
-						const namePattern = /^[A-Za-zА-Яа-яЁё\s-]+$/
-						if (!namePattern.test(value))
-							return "Имя должно содержать только буквы и пробелы"
-						return true
-					},
-				})}
-				placeholder="Ваше имя"
-				onChange={(e) => {
-					setValue("name", e.target.value)
-					trigger("name")
-				}}
-				autoComplete="given-name"
-				aria-invalid={errors.name ? "true" : "false"}
-				ref={callBackBtnRef}
-			/>
-			{errors.name && (
-				<p role="alert" className={styles.form__error}>
-					{errors.name?.message}
-				</p>
-			)}
-			<InputMask
-				className={`${styles.form__input} ${errors.phone?.message ? styles.resetIndent : ""}`}
-				mask="+375 (99) 999-99-99"
-				{...register("phone", {
-					required: "Телефон обязателен",
-					validate: (value) => {
-						const digits = value.replace(/\D/g, "")
-						const operatorCode = value.match(/\+375 \((\d{2})\)/)?.[1]
-						if (!operatorCode) return "Введите код оператора"
-						if (!["44", "25", "29", "33", "17"].includes(operatorCode))
-							return "Неверный код оператора"
-						if (digits.length < 12) return "Введите номер полностью"
-						return true
-					},
-				})}
-				autoComplete="off"
-				placeholder="Ваш номер телефона"
-				onClick={(e) => setPhoneInputCursor(e)}
-				onChange={(e) => {
-					setValue("phone", e.target.value)
-					trigger("phone")
-				}}
-				onBlur={() => setIsPhoneInputFocused(false)}
-				aria-invalid={errors.phone ? "true" : "false"}
-			/>
+			<div className={styles["form__input-wrapper"]}>
+				<input
+					className={`${styles.form__input} ${errors.name?.message ? styles.resetIndent : ""}`}
+					{...register("name", {
+						required: "Имя обязательно",
+						validate: (value) => {
+							if (value.length < 2)
+								return "Имя должно содержать хотя бы 2 символа"
+							const namePattern = /^[A-Za-zА-Яа-яЁё\s-]+$/
+							if (!namePattern.test(value))
+								return "Имя должно содержать только буквы и пробелы"
+							return true
+						},
+					})}
+					placeholder="Ваше имя"
+					onChange={(e) => {
+						setValue("name", e.target.value)
+						trigger("name")
+					}}
+					autoComplete="given-name"
+					aria-invalid={errors.name ? "true" : "false"}
+					ref={callBackBtnRef}
+				/>
+				{errors.name && (
+					<p role="alert" className={styles.form__error}>
+						{errors.name?.message}
+					</p>
+				)}
+			</div>
+			<div className={styles["form__input-wrapper"]}>
+				<InputMask
+					className={`${styles.form__input} ${errors.phone?.message ? styles.resetIndent : ""}`}
+					mask="+375 (99) 999-99-99"
+					{...register("phone", {
+						required: "Телефон обязателен",
+						validate: (value) => {
+							const digits = value.replace(/\D/g, "")
+							const operatorCode = value.match(/\+375 \((\d{2})\)/)?.[1]
+							if (!operatorCode && getLastDigitIndex(value) !== 1)
+								return "Введите код оператора"
+							if (!["44", "25", "29", "33", "17"].includes(operatorCode))
+								return "Неверный код оператора"
+							if (digits.length < 12) return "Введите номер полностью"
+							return true
+						},
+					})}
+					autoComplete="off"
+					placeholder="Ваш номер телефона"
+					onClick={(e) => setPhoneInputCursor(e)}
+					onChange={(e) => {
+						setValue("phone", e.target.value)
+						trigger("phone")
+					}}
+					onBlur={() => setIsPhoneInputFocused(false)}
+					aria-invalid={errors.phone ? "true" : "false"}
+				/>
+			</div>
 			{errors.phone && (
 				<p role="alert" className={styles.form__error}>
 					{errors.phone?.message}
 				</p>
 			)}
 			{isEmailShow && (
-				<input
-					className={`${styles.form__input} ${errors.email?.message ? styles.resetIndent : ""}`}
-					{...register("email", {
-						required: "Email обязателен",
-						pattern: {
-							value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-							message: "Неверный формат email",
-						},
-					})}
-					placeholder="Ваш email"
-					onChange={(e) => {
-						setValue("email", e.target.value)
-						trigger("email")
-					}}
-					aria-invalid={errors.email ? "true" : "false"}
-				/>
+				<div className={styles["form__input-wrapper"]}>
+					<input
+						className={`${styles.form__input} ${errors.email?.message ? styles.resetIndent : ""}`}
+						{...register("email", {
+							required: "Email обязателен",
+							pattern: {
+								value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+								message: "Неверный формат email",
+							},
+						})}
+						placeholder="Ваш email"
+						onChange={(e) => {
+							setValue("email", e.target.value)
+							trigger("email")
+						}}
+						aria-invalid={errors.email ? "true" : "false"}
+					/>
+				</div>
 			)}
 			{errors.email && (
 				<p role="alert" className={styles.form__error}>
 					{errors.email?.message}
 				</p>
 			)}
-			<textarea
-				className={`${styles.form__textarea} ${errors.text?.message ? styles.resetIndent : ""}`}
-				{...register("text", {
-					required: "Сообщение обязателено",
-					minLength: {
-						value: 5,
-						message: "Сообщение должно содержать минимум 5 символов",
-					},
-					maxLength: {
-						value: 500,
-						message: "Сообщение не может превышать 500 символов",
-					},
-					validate: (value) => {
-						const forbiddenChars = /[^A-Za-zА-Яа-яЁё\w\s.,!?-]/
-						if (forbiddenChars.test(value))
-							return "Сообщение содержит недопустимые символы"
-						return true
-					},
-				})}
-				placeholder="Текст сообщения"
-				onChange={(e) => {
-					setValue("text", e.target.value)
-					trigger("text")
-				}}
-				aria-invalid={errors.text ? "true" : "false"}
-			></textarea>
+			<div className={styles["form__textarea-wrapper"]}>
+				<textarea
+					className={`${styles.form__textarea} ${errors.text?.message ? styles.resetIndent : ""}`}
+					{...register("text", {
+						required: "Сообщение обязателено",
+						minLength: {
+							value: 5,
+							message: "Сообщение должно содержать минимум 5 символов",
+						},
+						maxLength: {
+							value: 500,
+							message: "Сообщение не может превышать 500 символов",
+						},
+						validate: (value) => {
+							const forbiddenChars = /[^A-Za-zА-Яа-яЁё\w\s.,!?-]/
+							if (forbiddenChars.test(value))
+								return "Сообщение содержит недопустимые символы"
+							return true
+						},
+					})}
+					placeholder="Текст сообщения"
+					onChange={(e) => {
+						setValue("text", e.target.value)
+						trigger("text")
+					}}
+					aria-invalid={errors.text ? "true" : "false"}
+				></textarea>
+			</div>
+
 			{errors.text && (
 				<p role="alert" className={styles.form__error}>
 					{errors.text?.message}
