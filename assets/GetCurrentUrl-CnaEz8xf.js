ddca99a4a920402051aca9d543822104b1c64b78
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-05-11 20:58:14 +0300

    Updates

---
+++ assets/GetCurrentUrl-CnaEz8xf.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-BBxuwDZ5.js";const e=()=>`https://w0x3r.github.io/Shower-Partitions${t().pathname}`;export{e as G};
--- assets/GetCurrentUrl-Lcgr5Fw4.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-BvszRmjX.js";const e=()=>`https://w0x3r.github.io/Shower-Partitions${t().pathname}`;export{e as G};
