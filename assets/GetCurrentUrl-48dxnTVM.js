573a0974eae6c462140c025f30fc6724be35af6c
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2025-05-30 13:50:57 +0300

    Updates

--- assets/GetCurrentUrl-48dxnTVM.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-C7oVZp8R.js";const e=()=>`https://w0x3r.github.io/Showers-Gomel${t().pathname}`;export{e as G};
---
+++ assets/GetCurrentUrl-Dot6STww.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-DUFXHmdZ.js";const e=()=>`https://w0x3r.github.io/Showers-Gomel${t().pathname}`;export{e as G};
